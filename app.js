7a4a906eb1f89f26d4e267fa917da759bac39979
Andrew Vy <andrew@andrewvy.com>
2012-10-02 06:45:50 -0700

    Added route, cleaned up boilerplate code.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
   require('./db');
@@ -31,7 +30,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/remove', routes.remove);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'ToDo List with Mongoose and Express', h1: 'ToDo List'});
 };
+
+exports.remove = function(req, res){
+  res.render('remove', {title: 'ToDo List with Mongoose and Express', h1: 'Remove an item!'})
+};
